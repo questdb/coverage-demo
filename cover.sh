9f4efe89d8a79a8cc4796df3aff38fcc66696658
Marko Topolnik <marko.topolnik@gmail.com>
2024-09-04 16:22:32 +0200

    Improve comments in cover.sh

--- cover.sh
+++ cover.sh
@@ -17,13 +17,13 @@ rustup component add llvm-tools-preview
 LLVM_TOOLS_PATH=$(dirname $(find $HOME/.rustup -name llvm-profdata))
 PATH=$LLVM_TOOLS_PATH:$PATH
 
-# Clean all build artifacts
+# Clean all build and coverage artifacts
 ./clean.sh
 
 # Maven test with coverage reporting
 mvn clean test
 
-# Process raw coverage data
+# Turn raw Rust coverage data into Cobertura format
 llvm-profdata merge -sparse -o rust-coverage.profdata maven-test.profraw rust/coverage-demo/cargo-test.profraw
 test_bin=$(find target/rust-maven-plugin/coverage-demo/debug/deps -type f $executable_filter | grep demo-)
 llvm-cov show --format=html --ignore-filename-regex='(.cargo/registry|rustc/.*\.rs)' \
@@ -40,6 +40,7 @@ llvm-cov export --format=lcov --ignore-filename-regex='(.cargo/registry|rustc/.*
   https://raw.githubusercontent.com/eriwen/lcov-to-cobertura-xml/master/lcov_cobertura/lcov_cobertura.py
 python3 lcov_cobertura.py rust-coverage.lcov --base-dir rust/coverage-demo/src --output rust-coverage.xml
 
+# Use Cover Checker to post the combined coverage report to the pull request
 java -jar lib/cover-checker-1.5.0-all.jar \
   -type jacoco --cover target/site/jacoco/jacoco.xml \
   -type cobertura --cover rust-coverage.xml \
