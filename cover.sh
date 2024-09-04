73e1880b485cd852b9c122febfb87d4e3d445644
Marko Topolnik <marko.topolnik@gmail.com>
2024-09-04 15:19:39 +0200

    Multiplatform script

--- cover.sh
+++ cover.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -eux
 
+if [ "$(uname)" -eq "Linux"]; then
+  executable_filter="-executable"
+  dylib_ext="so"
+else
+  # Assume MacOS
+  executable_filter="-perm +111"
+  dylib_ext="dylib"
+fi
+
 cd $(dirname $0)
 
 # Install LLVM coverage tools
@@ -16,16 +25,16 @@ LLVM_PROFILE_FILE=maven-test.profraw mvn clean test
 
 # Process raw coverage data
 llvm-profdata merge -sparse -o rust-coverage.profdata maven-test.profraw rust/coverage-demo/cargo-test.profraw
-test_bin=$(find target/rust-maven-plugin/coverage-demo/debug/deps -type f -perm +111 | grep demo-)
+test_bin=$(find target/rust-maven-plugin/coverage-demo/debug/deps -type f $executable_filter | grep demo-)
 llvm-cov show --format=html --ignore-filename-regex='(.cargo/registry|rustc/.*\.rs)' \
   --instr-profile rust-coverage.profdata \
   --object $test_bin \
-  --object target/classes/org/example/libcoverage_demo.dylib \
+  --object target/classes/org/example/libcoverage_demo.$dylib_ext \
   --output-dir=rust-coverage
 llvm-cov export --format=lcov --ignore-filename-regex='(.cargo/registry|rustc/.*\.rs)' \
   --instr-profile rust-coverage.profdata \
   --object $test_bin \
-  --object target/classes/org/example/libcoverage_demo.dylib \
+  --object target/classes/org/example/libcoverage_demo.$dylib_ext \
   > rust-coverage.lcov
 [ ! -f lcov_cobertura.py ] && wget \
   https://raw.githubusercontent.com/eriwen/lcov-to-cobertura-xml/master/lcov_cobertura/lcov_cobertura.py
